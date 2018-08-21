215ea7cebaf449d2376054310f5bc2870a573007
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-21 21:28:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,17 +25,24 @@ class Passenger{
 
 
 
-
-class Trips{
-  constructor(driverId, passengerId){
-   this.driverId = driver.id;
+class Trip {
+  constructor(driver, passenger) {
+    this.driverId = driver.id;
     this.passengerId = passenger.id;
     this.id = tripId++;
     store.trips.push(this);
-   }
+  }
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId;
+    });
+  }
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId;
+    });
+  }
 }
-
-
 
 
 
