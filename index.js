e93ad67e7e21d0f686a5b16e4622b0de45f4e4e2
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-21 21:29:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
+// driver has many trips
+// passenger has many trips
+// trips
+//// passenger
+//// driver
+
 let store = { drivers: [], passengers: [], trips: [] };
 
-let id=0;
-let driverId=0; 
-let passengerId=0;
-class Driver{
-  constructor(name){
-  this.name = name;
-  store.drivers.push(this);
-this.id++;
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+
+class Driver {
+  constructor(name) {
+    this.name = name;
+    this.id = driverId++;
+    store.drivers.push(this);
   }
-  
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId == this.id;
+    });
+  }
+  passengers() {
+    return this.trips().map(trip => {
+      return trip.passenger();
+    });
+  }
 }
 
-
-
-class Passenger{
-  constructor(name){
-  this.name = name;
-  store.passengers.push(this);
-  this.id++;
+class Passenger {
+  constructor(name) {
+    this.name = name;
+    this.id = passengerId++;
+    store.passengers.push(this);
   }
-  
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId == this.id;
+    });
+  }
+  drivers() {
+    return this.trips().map(trip => {
+      return trip.driver();
+    });
+  }
 }
-
-
 
 class Trip {
   constructor(driver, passenger) {
@@ -43,7 +65,3 @@ class Trip {
     });
   }
 }
-
-
-
-
